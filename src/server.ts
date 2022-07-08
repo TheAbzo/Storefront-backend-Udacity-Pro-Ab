08620298ff94ca2e00c0c460f4d8c3126f32b229
TheAbzo <abdelrahman.elferlay@gmail.com>
2022-07-08 18:00:55 +0200

    fix: fixing a bug in conf

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express'
 import bodyParser from 'body-parser'
 
 const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const address: string = "0.0.0.0:3002"
 
 app.use(bodyParser.json())
 
@@ -10,6 +10,6 @@ app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+app.listen(3002, function () {
     console.log(`starting app on: ${address}`)
 })
